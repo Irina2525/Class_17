c556e5f4fc23b62d601da2dfe594b3df8e712bbd
Irina <spartak.irina92@gmail.com>
2022-01-07 17:56:15 +0700

    Добавьте файлы проекта.

---
+++ Class_17/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_17
+{
+    class BankAccount<T>
+    {
+        public T NumberAccount { get; set; }  // номер счет
+        public int BalanceAccount { get; set; } // баланс счета
+        public string Name { get; set; } // ФИО
+
+        //public BankAccount(T number, int balance, string name) // добавляем коструктор , кт принимает 3 значения и заполняет соот-ие св-ва
+        //{
+        //    NumberAccount = number;
+        //    BalanceAccount = balance;
+        //    Name = name;
+        //}
+        public string GetInfo() // метод кт возвращает строку
+        {
+            return $"{NumberAccount} {BalanceAccount} {Name}";
+        }
+        public void Input() // метод кт запрашивает значения 
+        {
+            Console.WriteLine("Введите номера счета в банке:");
+            string stringNumber = Console.ReadLine();
+            NumberAccount = (T)Convert.ChangeType(stringNumber, typeof(T)); // typeof принимает тип и возвращает информацию, приводит к нужному типу
+
+            Console.WriteLine("Введите баланс счета в банке:");
+            BalanceAccount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счета в банке:");
+            Name = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Class_17/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Class_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите числовой номера счета в банке:");
+            //int numberInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите баланс счета в банке:");
+            //int balance = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите ФИО владельца счета в банке:");
+            //string name = Console.ReadLine();
+            //BankAccount<int> bankAccount1 = new BankAccount<int>(numberInt, balance, name);
+            //Console.WriteLine(bankAccount1.GetInfo()); // у экземпляра класса вызываем метод GetInfo для вывода
+
+            //Console.WriteLine();
+            //Console.WriteLine("Введите строковый номера счета в банке:");
+            //string numberString = Console.ReadLine();
+            //Console.WriteLine("Введите баланс счета в банке:");
+            //balance = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите ФИО владельца счета в банке:");
+            //name = Console.ReadLine();
+            //BankAccount<string> bankAccount2 = new BankAccount<string>(numberString, balance, name);
+            //Console.WriteLine(bankAccount2.GetInfo());
+
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            Console.WriteLine(bankAccount1.GetInfo());
+            Console.WriteLine();
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            Console.WriteLine(bankAccount2.GetInfo());
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
